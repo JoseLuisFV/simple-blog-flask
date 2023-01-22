d7b1a886a83642a2a61c81f154561061038b7a27
BIOZ <josefvacio@gmail.com>
2023-01-22 15:59:49 -0800

    Fixed some bugs on get_post in blog, and fixed bug on load user in auth

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -6,17 +6,27 @@ from werkzeug.exceptions import abort
 from flaskr.auth import login_required
 from flaskr.db import get_db
 
+import markdown as md
+
 
 bp = Blueprint('blog', __name__)
 
 @bp.route('/')
 def index():
     db = get_db()
-    posts = db.execute(
+    db_posts = db.execute(
         'SELECT p.id, title, body, created, author_id, username'
         ' FROM post p JOIN user u ON p.author_id = u.id'
         ' ORDER BY created DESC'
     ).fetchall()
+
+    posts = []
+
+    for post in db_posts:
+        post = dict(post)
+        post['body'] = md.markdown(post['body'])
+        posts.append(post)
+
     return render_template('blog/index.html', posts=posts)
 
 
